f02890ef7f2771f68d2ed8f7221f0e66337663ee
Anuj Kundar <anujkundar003@gmail.com>
2025-09-23 19:21:24 +0530

    add get bean by type: name and class

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -12,11 +12,11 @@ public class App
     public static void main(String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj= (Alien) context.getBean("alien");
+        Alien obj= context.getBean("alien",Alien.class);
 //        obj.setAge(12);
         System.out.println(obj.getAge());
         obj.code();
 
-        Desktop obj1= (Desktop) context.getBean("comp2");
+        Desktop obj1= context.getBean(Desktop.class);
     }
 }
